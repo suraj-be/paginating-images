6ce39efae5f93eccfdac6a68b2372bea3ada97ab
suraj.be <suraj.be>
2020-11-22 10:51:29 +0530

    added somewhat hack for many paged items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,7 +15,67 @@ export class AppComponent {
       "url": "assets/buildings.jpg"
     },
     {
-      "title": "Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.",
+      "title": "Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus. Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus. Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus. ",
+      "url": "assets/forest.jpg"
+    },
+    {
+      "title": "Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.\n\nCras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\n\nProin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.",
+      "url": "assets/laptop.jpg"
+    },
+    {
+      "title": "Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+      "url": "assets/mountains.jpg"
+    },
+    {
+      "title": "Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.\n\nMaecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.\n\nCurabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.",
+      "url": "assets/scenery.jpg"
+    },
+    {
+      "title": "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.\n\nAliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.\n\nSed ante. Vivamus tortor. Duis mattis egestas metus.",
+      "url": "assets/buildings.jpg"
+    },
+    {
+      "title": "Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus. Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus. Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus. ",
+      "url": "assets/forest.jpg"
+    },
+    {
+      "title": "Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.\n\nCras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\n\nProin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.",
+      "url": "assets/laptop.jpg"
+    },
+    {
+      "title": "Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+      "url": "assets/mountains.jpg"
+    },
+    {
+      "title": "Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.\n\nMaecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.\n\nCurabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.",
+      "url": "assets/scenery.jpg"
+    },
+    {
+      "title": "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.\n\nAliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.\n\nSed ante. Vivamus tortor. Duis mattis egestas metus.",
+      "url": "assets/buildings.jpg"
+    },
+    {
+      "title": "Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus. Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus. Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus. ",
+      "url": "assets/forest.jpg"
+    },
+    {
+      "title": "Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.\n\nCras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\n\nProin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.",
+      "url": "assets/laptop.jpg"
+    },
+    {
+      "title": "Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+      "url": "assets/mountains.jpg"
+    },
+    {
+      "title": "Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.\n\nMaecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.\n\nCurabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.",
+      "url": "assets/scenery.jpg"
+    },
+    {
+      "title": "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.\n\nAliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.\n\nSed ante. Vivamus tortor. Duis mattis egestas metus.",
+      "url": "assets/buildings.jpg"
+    },
+    {
+      "title": "Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus. Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus. Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus. ",
       "url": "assets/forest.jpg"
     },
     {
@@ -31,4 +91,8 @@ export class AppComponent {
       "url": "assets/scenery.jpg"
     }
   ]
+
+  checkWindowIndex(idx) {
+    return (Math.abs(this.currentPage - idx) < 5);
+  }
 }
