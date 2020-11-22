0beb1d4960e830073a2643e2f44cec29c8ecbbe1
suraj.be <suraj.be>
2020-11-22 10:19:04 +0530

    added local images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,23 +12,23 @@ export class AppComponent {
   images = [
     {
       "title": "In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.\n\nAliquam quis turpis eget elit sodales scelerisque. Mauris sit amet eros. Suspendisse accumsan tortor quis turpis.\n\nSed ante. Vivamus tortor. Duis mattis egestas metus.",
-      "url": "https://media.istockphoto.com/photos/fresh-fruits-and-vegetables-picture-id589415708"
+      "url": "assets/buildings.jpg"
     },
     {
       "title": "Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus.",
-      "url": "https://media.istockphoto.com/photos/fresh-fruits-and-vegetables-in-the-basket-picture-id541822810"
+      "url": "assets/forest.jpg"
     },
     {
       "title": "Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio.\n\nCras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.\n\nProin interdum mauris non ligula pellentesque ultrices. Phasellus id sapien in sapien iaculis congue. Vivamus metus arcu, adipiscing molestie, hendrerit at, vulputate vitae, nisl.",
-      "url": "https://media.istockphoto.com/photos/healthy-fresh-rainbow-colored-fruits-and-vegetables-background-picture-id1208790371"
+      "url": "assets/laptop.jpg"
     },
     {
       "title": "Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
-      "url": "https://media.istockphoto.com/photos/assortment-of-colorful-ripe-tropical-fruits-top-view-picture-id995518546"
+      "url": "assets/mountains.jpg"
     },
     {
       "title": "Suspendisse potenti. In eleifend quam a odio. In hac habitasse platea dictumst.\n\nMaecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat.\n\nCurabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem.",
-      "url": "https://media.istockphoto.com/photos/shopping-bag-full-of-fresh-vegetables-and-fruits-picture-id1128687123"
+      "url": "assets/scenery.jpg"
     }
   ]
 }
